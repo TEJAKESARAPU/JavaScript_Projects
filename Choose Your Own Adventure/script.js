95c4b8b23f089ef6af0384bdeeb0e7197d428539
TEJA KESARAPU <145187633+TEJAKESARAPU@users.noreply.github.com>
2024-07-13 21:40:42 +0530

    Create script.js

---
+++ Choose Your Own Adventure/script.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")();
+
+const answer = prompt("Would you like to play (y/n)? ")
+
+if (answer.toLowerCase() === "y") {
+    const answer2 = prompt("Would you like to left or right (left/right)? ")
+
+    if (answer2.toLowerCase() === "left" ) {
+        console.log("Oops, you go to left and fall of a bridge! You lost!");
+    } else {
+        //right 
+        console.log("Nice! You go right and approach a bridge...")
+        const answer3 = prompt("Would you like to cross the bridge, or turn and find another way (cross/turn)? ");
+
+        if (answer3 === "cross") {
+            console.log("You cross the bridge and reach the end of the game!")
+        } else {
+            console.log("You turn back and trip on a log and hurt your leg. You lose.");
+        }
+    }
+} else {
+    console.log("That's too bad!");
+}
